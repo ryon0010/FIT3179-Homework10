ec0d08c6dd6de996fdf46bb3fd9f6fc13a627551
ryon0010 <131249868+ryon0010@users.noreply.github.com>
2024-10-09 00:25:38 +0800

    final changes

---
+++ script.js
@@ -0,0 +1,11 @@
+var vg_1 = "map_vis.json";
+vegaEmbed("#map", vg_1)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_2 = "line_vis.json";
+vegaEmbed("#line", vg_2)
+    .then(function (result) {})
+    .catch(console.error);
+    
+
